6c6412651d24153772289b74fb68f1361f7edbb7
minhdangnsp <dangnguyentranminh@gmail.com>
2024-09-11 00:21:15 +0700

    b147: Thực hành Register Form với RHF - Styling

---
+++ src/hooks/form/RegisterHook.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RegisterHook = () => {
+    return (
+        <div className=' max-w-[300px] mx-auto my-10'>
+            <div className="flex flex-col gap-3">
+                <label htmlFor="username" className=' cursor-pointer'>Username</label>
+                <input type="text" placeholder="Enter your username" id="username" className="p-4 transition-all bg-white border border-gray-100 rounded-lg outline-none focus:border-blue-500"/>
+                <p className=' text-red-500 text-sm'>Please enter your username</p>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterHook;
