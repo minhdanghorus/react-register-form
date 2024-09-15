92941cceb8d7a9d45ca1ef2311e1be677eac88d1
minhdangnsp <dangnguyentranminh@gmail.com>
2024-09-15 22:16:29 +0700

    b151: Thực hành Register Form với RHF - Custom Dropdown phần 1

--- src/components/dropdown/DropdownHook.jsx
+++ src/components/dropdown/DropdownHook.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React from "react";
+import useClickOutSide from "../../hooks/useClickOutSide";
 
-const DropdownHook = () => {
-    return (
-        <div className=' relative'>
-            <div className="p-5 rounded-lg border border-gray-100 bg-white flex items-center justify-between cursor-pointer">
-                <span>Select your job</span>
-            </div>
-            <div className=" absolute top-full left-0 w-full rounded-lg bg-white">
-                <div className="p-5">Teacher</div>
-                <div className="p-5">Student</div>
-                <div className="p-5">Doctor</div>
-            </div>
-        </div>
-    );
+const DropdownHook = ({control}) => {
+  const { show, setShow, nodeRef } = useClickOutSide();
+  return (
+    <div className=" relative" ref={nodeRef}>
+      <div
+        className="p-5 rounded-lg border border-gray-100 bg-white flex items-center justify-between cursor-pointer"
+        onClick={() => setShow(!show)}
+      >
+        <span>Select your job</span>
+      </div>
+      <div
+        className={`absolute top-full left-0 w-full rounded-lg bg-white ${
+          show ? "" : "opacity-0 invisible"
+        }`}
+      >
+        <div className="p-5">Teacher</div>
+        <div className="p-5">Student</div>
+        <div className="p-5">Doctor</div>
+      </div>
+    </div>
+  );
 };
 
 export default DropdownHook;
